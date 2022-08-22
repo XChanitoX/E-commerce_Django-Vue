fb1ab229418b181a8f225cdce3b2b59a9db8b547
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2022-08-22 16:06:34 -0500

    Creamos el carrito como componente

--- ecommerce_front/src/router/index.js
+++ ecommerce_front/src/router/index.js
@@ -4,12 +4,18 @@ import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
 import CategoryView from '../views/CategoryView.vue'
 import SearchView from '../views/SearchView.vue'
+import CartView from '../views/CartView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: CartView
   },
   {
     path: '/search',
